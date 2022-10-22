1c02186777aac1d6359076c844faf42647d589a6
Mohamed Ait Lahcen <mohamedaitlahcen@gmail.com>
2022-10-22 14:53:49 +0100

    create the interface of openFeign to get data from Customer service

--- billing-service/src/main/java/com/example/billingservice/openfeigh/CustomerServiceRestClient.java
+++ billing-service/src/main/java/com/example/billingservice/openfeigh/CustomerServiceRestClient.java
@@ -1,4 +1,19 @@
 package com.example.billingservice.openfeigh;
 
+import com.example.billingservice.entities.Customer;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@FeignClient(name = "CUSTOMER-SERVICE")
 public interface CustomerServiceRestClient {
+    //recuperer la liste des customers
+    @GetMapping(path = "/api/customers")
+    public List<Customer> getCustomers();
+
+    @GetMapping(path = "/api/customers/{id}")
+    public Customer getCustomer(@PathVariable(name = "id") String id);
+
 }
