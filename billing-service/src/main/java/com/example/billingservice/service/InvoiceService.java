af501c1b77bd62f4673454fef023bc7a8c60f338
Mohamed Ait Lahcen <mohamedaitlahcen@gmail.com>
2022-10-22 15:13:07 +0100

    create invoiceService interface

---
+++ billing-service/src/main/java/com/example/billingservice/service/InvoiceService.java
@@ -0,0 +1,14 @@
+package com.example.billingservice.service;
+
+import com.example.billingservice.dtos.InvoiceRequestDTO;
+import com.example.billingservice.dtos.InvoiceResponseDTO;
+import com.example.billingservice.entities.Customer;
+import com.example.billingservice.entities.Invoice;
+
+import java.util.List;
+
+public interface InvoiceService {
+     InvoiceResponseDTO save(InvoiceRequestDTO invoiceRequestDTO);
+     InvoiceResponseDTO getInvoice(String invoiceId);
+     List<InvoiceResponseDTO> getInvoicesByCustomerId(String customerId);
+}
