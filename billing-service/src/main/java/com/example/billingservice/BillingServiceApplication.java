1e3c3a48fc1af3c9a931dc54e7fe5ca1b648b45e
Mohamed Ait Lahcen <mohamedaitlahcen@gmail.com>
2022-10-23 09:39:06 +0100

    add some data tows to invoice in table in database

--- billing-service/src/main/java/com/example/billingservice/BillingServiceApplication.java
+++ billing-service/src/main/java/com/example/billingservice/BillingServiceApplication.java
@@ -1,8 +1,14 @@
 package com.example.billingservice;
 
+import com.example.billingservice.dtos.InvoiceRequestDTO;
+import com.example.billingservice.service.InvoiceService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+
+import java.math.BigDecimal;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -10,6 +16,15 @@ public class BillingServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BillingServiceApplication.class, args);
+    }
+    @Bean
+    CommandLineRunner commandLineRunner(InvoiceService invoiceService){
+        return args->{
+          invoiceService.save(new InvoiceRequestDTO(new BigDecimal(758884),"C01"));
+          invoiceService.save(new InvoiceRequestDTO(new BigDecimal(550184),"C01"));
+          invoiceService.save(new InvoiceRequestDTO(new BigDecimal(484521),"C02"));
+
+        };
     }
 
 }
