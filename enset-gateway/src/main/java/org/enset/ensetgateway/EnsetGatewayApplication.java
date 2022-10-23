814ee3e75a62f7c4e5bd0ae8bd2669302be002a1
Mohamed Ait Lahcen <mohamedaitlahcen@gmail.com>
2022-10-23 12:29:01 +0100

    create a method inside gateway to manage trafic

--- enset-gateway/src/main/java/org/enset/ensetgateway/EnsetGatewayApplication.java
+++ enset-gateway/src/main/java/org/enset/ensetgateway/EnsetGatewayApplication.java
@@ -2,12 +2,20 @@ package org.enset.ensetgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class EnsetGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EnsetGatewayApplication.class, args);
+	}
+	@Bean
+	DiscoveryClientRouteDefinitionLocator discoveryClientRouteDefinitionLocator(ReactiveDiscoveryClient rdc, DiscoveryLocatorProperties dlp){
+		return new DiscoveryClientRouteDefinitionLocator(rdc,dlp);
 	}
 
 }
