0365a3ece64367a4cdc92426bfd864300c318369
Mohamed Ait Lahcen <mohamedaitlahcen@gmail.com>
2022-10-23 09:43:07 +0100

    create eureka-discovery-service

---
+++ enset-eureka-service/src/main/java/org/enset/enseteurekaservice/EnsetEurekaServiceApplication.java
@@ -0,0 +1,14 @@
+package org.enset.enseteurekaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@En
+public class EnsetEurekaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnsetEurekaServiceApplication.class, args);
+	}
+
+}
---
+++ enset-eureka-service/src/test/java/org/enset/enseteurekaservice/EnsetEurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.enset.enseteurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnsetEurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
